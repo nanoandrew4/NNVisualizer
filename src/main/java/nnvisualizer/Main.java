9b0d3446f9576ea8a152101ab758c3f92f464c1a
nanoandrew4 <nanoandrew4@gmail.com>
2019-09-16 22:10:11 +0200

    Implemented visualization for weights between neurons, which are grayscale depending on how much influence they exert. Also implemented moving between network generations using arrow keys

--- src/main/java/nnvisualizer/Main.java
+++ src/main/java/nnvisualizer/Main.java
@@ -2,8 +2,11 @@ package nnvisualizer;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
 import nnvisualizer.nn.FFNeuralNetwork;
@@ -13,11 +16,18 @@ import nnvisualizer.renderer.NNRenderer;
 
 public class Main extends Application {
 
+	public final static double SCREEN_WIDTH = Screen.getPrimary().getBounds().getWidth();
+	public final static double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
+
 	private static NNRenderer nnRenderer;
 
 	static NeuralNetwork[] neuralNetworks;
 
-	private Pane pane;
+	private int renderedNetwork = 0;
+
+	private Pane globalPane, neuralNetworkPane;
+
+	private Text generationCounter = new Text(SCREEN_WIDTH - SCREEN_WIDTH / 8d, SCREEN_HEIGHT / 50d, "Generation: 0");
 
 	public static void main(final String[] args) {
 		if (args.length == 2 && Integer.parseInt(args[0]) == 0) {
@@ -28,12 +38,27 @@ public class Main extends Application {
 	}
 
 	@Override
-	public void start(final Stage stage) throws Exception {
-		final Scene scene = new Scene(pane = new Pane(), Screen.getPrimary().getBounds().getWidth(), Screen.getPrimary().getBounds().getHeight(), Color.WHITE);
+	public void start(final Stage stage) {
+		globalPane = new Pane(generationCounter);
+		generationCounter.setFont(new Font(SCREEN_HEIGHT / 50d));
+
+		neuralNetworkPane = new Pane();
+		globalPane.getChildren().add(neuralNetworkPane);
+
+		final Scene scene = new Scene(globalPane, SCREEN_WIDTH, SCREEN_HEIGHT, Color.WHITE);
 		stage.setScene(scene);
 		stage.setTitle("NNVisualizer");
 
-		nnRenderer.renderNetwork(pane, neuralNetworks[0]);
+		nnRenderer.renderNetwork(neuralNetworkPane, neuralNetworks[0]);
+
+		scene.setOnKeyReleased(keyEvent -> {
+			if (keyEvent.getCode() == KeyCode.LEFT && renderedNetwork > 0) {
+				nnRenderer.renderNetwork(neuralNetworkPane, neuralNetworks[--renderedNetwork]);
+			} else if (keyEvent.getCode() == KeyCode.RIGHT && renderedNetwork < neuralNetworks.length - 1) {
+				nnRenderer.renderNetwork(neuralNetworkPane, neuralNetworks[++renderedNetwork]);
+			}
+			generationCounter.setText("Generation: " + renderedNetwork);
+		});
 
 		stage.show();
 	}
--- src/main/java/nnvisualizer/renderer/FFNNRenderer.java
+++ src/main/java/nnvisualizer/renderer/FFNNRenderer.java
@@ -1,35 +1,46 @@
 package nnvisualizer.renderer;
 
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
-import javafx.stage.Screen;
+import javafx.scene.shape.Line;
+import nnvisualizer.Main;
 import nnvisualizer.nn.FFNeuralNetwork;
 import nnvisualizer.nn.NeuralNetwork;
 
 public class FFNNRenderer implements NNRenderer {
-
-	private final static double SCREEN_WIDTH = Screen.getPrimary().getBounds().getWidth();
-	private final static double SCREEN_HEIGHT = Screen.getPrimary().getBounds().getHeight();
 
 	@Override
 	public void renderNetwork(final Pane pane, final NeuralNetwork neuralNetwork) {
 		pane.getChildren().clear();
 		final FFNeuralNetwork ffNeuralNetwork = (FFNeuralNetwork) neuralNetwork;
 
+		final Circle[][] neurons = new Circle[ffNeuralNetwork.getActivation().length][];
 		for (int l = 0; l < ffNeuralNetwork.getActivation().length; l++) {
+			neurons[l] = new Circle[ffNeuralNetwork.getActivation()[l].length];
 			for (int n = 0; n < ffNeuralNetwork.getActivation()[l].length; n++) {
-				final Circle neuron = generateCircle(ffNeuralNetwork, l, n);
-				pane.getChildren().add(neuron);
-				// Draw weights as lines
+				neurons[l][n] = generateCircle(ffNeuralNetwork, l, n);
+				pane.getChildren().add(neurons[l][n]);
+
+				if (l > 0) {
+					for (int pn = 0; pn < ffNeuralNetwork.getActivation()[l - 1].length; pn++) {
+						final Circle startNeuron = neurons[l - 1][pn];
+						final Circle endNeuron = neurons[l][n];
+						final Line line = new Line(startNeuron.getCenterX() + startNeuron.getRadius(), startNeuron.getCenterY(),
+												   endNeuron.getCenterX() - endNeuron.getRadius(), endNeuron.getCenterY());
+						line.setStroke(Color.gray(1 - ffNeuralNetwork.getWeights()[l - 1][pn][n])); // TODO: NORMALIZE VALUES
+						pane.getChildren().add(line);
+					}
+				}
 			}
 		}
 	}
 
 	private Circle generateCircle(final FFNeuralNetwork ffNeuralNetwork, int layer, int neuronInLayer) {
-		final Circle neuron = new Circle(SCREEN_HEIGHT / (ffNeuralNetwork.getActivation()[0].length * 4d));
-		final double circleRadiiMaxWidth = (SCREEN_WIDTH - neuron.getRadius() * 2);
-		final double circleRadiiMaxHeight = (SCREEN_HEIGHT - neuron.getRadius() * 2);
+		final Circle neuron = new Circle(Main.SCREEN_HEIGHT / (ffNeuralNetwork.getActivation()[0].length * 4d));
+		final double circleRadiiMaxWidth = (Main.SCREEN_WIDTH - neuron.getRadius() * 2);
+		final double circleRadiiMaxHeight = (Main.SCREEN_HEIGHT - neuron.getRadius() * 2);
 
 		double cx = (((double) layer / ffNeuralNetwork.getActivation().length)) * circleRadiiMaxWidth;
 		double cy = (((double) neuronInLayer / ffNeuralNetwork.getActivation()[layer].length)) * circleRadiiMaxHeight;
